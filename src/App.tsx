dfa76b7ff67cf3e0f794e8ffe9fa68798cb0e43e
matveykapljr11-alt <matvey.kapljr11@gmail.com>
2026-05-14 18:13:52 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { SiteHeader } from './components/SiteHeader'
 import HomePage from './pages/HomePage'
 import PrakiPage from './pages/PrakiPage'
 import TeamsPage from './pages/TeamsPage'
+import TeamProfilePage from './pages/TeamProfilePage'
 import PlayersPage from './pages/PlayersPage'
 import TournamentsPage from './pages/TournamentsPage'
 import ProfilePage from './pages/ProfilePage'
@@ -20,6 +21,7 @@ export default function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/praki" element={<PrakiPage />} />
           <Route path="/teams" element={<TeamsPage />} />
+          <Route path="/teams/:id" element={<TeamProfilePage />} />
           <Route path="/players" element={<PlayersPage />} />
           <Route path="/tournaments" element={<TournamentsPage />} />
           <Route path="/profile" element={<ProfilePage />} />
